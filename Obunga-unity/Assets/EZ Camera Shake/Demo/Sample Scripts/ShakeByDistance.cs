39429546ac43849ea4932c6bc7bc71e9c908bef8
Matvey <67281212+matvey733@users.noreply.github.com>
2023-03-12 16:16:51 +0300

    Update variable names
    forgor

--- Obunga-unity/Assets/EZ Camera Shake/Demo/Sample Scripts/ShakeByDistance.cs
+++ Obunga-unity/Assets/EZ Camera Shake/Demo/Sample Scripts/ShakeByDistance.cs
@@ -1,9 +1,9 @@
-﻿using UnityEngine;
+using UnityEngine;
 using EZCameraShake;
 
 /*
  * Shake the camera based on the player object's distance to the object this script is attached to.
- */
+*/
 public class ShakeByDistance : MonoBehaviour
 {
     public GameObject Player;
@@ -20,6 +20,6 @@ public class ShakeByDistance : MonoBehaviour
         float distanceFromPlayerToObject = Vector3.Distance(Player.transform.position, this.transform.position);
 
         // Scale the magnitude of the saved shake, so that the scale is higher the closer we get to the object.
-        _shakeInstance.ScaleMagnitude = 1 - Mathf.Clamp01(currentDistance / Distance);
+        _shakeInstance.ScaleMagnitude = 1 - Mathf.Clamp01(distanceFromPlayerToObject / MaxDistance);
 	}
 }
